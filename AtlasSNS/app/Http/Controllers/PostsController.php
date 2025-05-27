813c6bdb075741936697ce8c1499190408e00249
LAPTOP-EV2MC11S\ktakw <lull.koutakuwamura@gmail.com>
2025-05-27 20:38:55 +0900

    投稿機能：フォローしている人と自分だけの投稿が表示

--- AtlasSNS/app/Http/Controllers/PostsController.php
+++ AtlasSNS/app/Http/Controllers/PostsController.php
@@ -9,46 +9,67 @@ use Auth;
 
 class PostsController extends Controller
 {
-    //
-
-
+    /**
+     * 投稿一覧ページの表示
+     */
     public function index()
     {
-        $posts = Post::get(); //Postモデル（postsテーブル）からレコード情報を取得
-        $follows = Follow::get();
-        return view('posts.index', ['posts' => $posts, 'follows' => $follows]);
+        // ログインユーザーのID取得
+        $userId = Auth::id();
 
+        // フォローしているユーザーのID一覧を取得
+        $followedUserIds = Follow::where('following_id', $userId)->pluck('followed_id')->toArray();
+
+        // 自分のIDも追加
+        $followedUserIds[] = $userId;
+
+        // フォローしているユーザー + 自分 の投稿を取得
+        $posts = Post::with('user')
+            ->whereIn('user_id', $followedUserIds)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return view('posts.index', ['posts' => $posts]);
     }
 
+    /**
+     * 新規投稿ページの表示（※現時点では未使用かも）
+     */
     public function create()
     {
         return view('posts.create');
     }
 
+    /**
+     * 新規投稿を保存する処理
+     */
     public function store(Request $request)
     {
-
+        // フォームから送られてきた投稿内容を取得
         $post = $request->input('newPost');
+
+        // 現在ログイン中のユーザーIDを取得
         $user_id = Auth::id();
 
+        // 新しい投稿を作成（DBに保存）
         Post::create([
-
             'user_id' => $user_id,
-
             'post' => $post,
-
         ]);
+
+        // 元のページにリダイレクト（投稿完了後）
         return back();
-
-
     }
 
+    /**
+     * 投稿の削除処理
+     */
     public function delete($id)
     {
+        // 指定されたIDの投稿を削除
         Post::where('id', $id)->delete();
+
+        // 削除後に元のページに戻る
         return back();
-
     }
-
-
 }
--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -2,47 +2,49 @@
 
 @section('content')
 
-<!-- <from action="{{ url('/post')}}" method="POST">
-  @csrf
+    <!-- <from action="{{ url('/post')}}" method="POST">
+                          @csrf
 
-  <input type="hidden" name="_token" value="{{ csrf_token()}}">
-  <div>
-    <input type="text" name="post" value="" class="form-control" placeholder="内容の入力" required> </div>
-<button type="submit" class="btn btn-success pull-right">送信</button>
-</from>
--->
-{!! Form::open(['url' => '/post']) !!}
-{{Form::token()}}
-
-
-
-<div class="form-group">
-    <img src="/storage/images/{{ Auth::user()->images }}">
-    {{ Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください']) }}
-
-</div>
-<button type="submit">
-    <img src="/images/post.png">
-</button>
-
-<table class="table table-hover">
-
-    @foreach ($posts as $post)
-
-        <img src="/storage/images/{{ $post->user->images }}">
-        <p>{{ $post->user->username }}</p>
-        <p>{{ $post->post }}</p>
-        <p>{{ $post->created_at->format('Y-m-d H:i') }}</p>
-        <a href="/post/{{$post->id}}/delete" onclick="return confirm('こちらの本を削除してもよろしいでしょうか？')">
-            <!-- <img src="/images/trash-h.png"> -->
-        </a>
-
-    @endforeach
-
-</table>
+                          <input type="hidden" name="_token" value="{{ csrf_token()}}">
+                          <div>
+                            <input type="text" name="post" value="" class="form-control" placeholder="内容の入力" required> </div>
+                        <button type="submit" class="btn btn-success pull-right">送信</button>
+                        </from>
+                        -->
+    {!! Form::open(['url' => '/post']) !!}
+    {{Form::token()}}
 
 
-{!! Form::close() !!}
+
+    <div class="form-group">
+        <img src="/storage/images/{{ Auth::user()->images }}">
+        @csrf
+
+        {{ Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください']) }}
+
+    </div>
+    <button type="submit">
+        <img src="/images/post.png">
+    </button>
+
+    <table class="table table-hover">
+
+        @foreach ($posts as $post)
+
+            <img src="/storage/images/{{ $post->user->images }}">
+            <p>{{ $post->user->username }}</p>
+            <p>{{ $post->post }}</p>
+            <p>{{ $post->created_at->format('Y-m-d H:i') }}</p>
+            <a href="/post/{{$post->id}}/delete" onclick="return confirm('こちらの本を削除してもよろしいでしょうか？')">
+                <!-- <img src="/images/trash-h.png"> -->
+            </a>
+
+        @endforeach
+
+    </table>
+
+
+    {!! Form::close() !!}
 
 
 
